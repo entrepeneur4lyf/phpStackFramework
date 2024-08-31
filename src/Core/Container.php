8edf57f3fc244833b64673c1d631f2f74c0c5560
Shawn McAllister <shawn.payments@gmail.com>
2024-08-30 21:59:37 -0400

    feat: Add Container class for dependency injection

---
+++ src/Core/Container.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace phpStack\Core;
+
+use Closure;
+use ReflectionClass;
+use ReflectionParameter;
+use Exception;
+
+class Container
+{
+    protected $bindings = [];
+    protected $instances = [];
+
+    public function bind($abstract, $concrete = null, $shared = false)
+    {
+        if (is_null($concrete)) {
+            $concrete = $abstract;
+        }
+
+        if (!$concrete instanceof Closure) {
+            $concrete = $this->getClosure($abstract, $concrete);
+        }
+
+        $this->bindings[$abstract] = compact('concrete', 'shared');
+    }
+
+    public function singleton($abstract, $concrete = null)
+    {
+        $this->bind($abstract, $concrete, true);
+    }
+
+    public function make($abstract, array $parameters = [])
+    {
+        if (isset($this->instances[$abstract])) {
+            return $this->instances[$abstract];
+        }
+
+        $concrete = $this->getConcrete($abstract);
+
+        if ($this->isBuildable($concrete, $abstract)) {
+            $object = $this->build($concrete, $parameters);
+        } else {
+            $object = $this->make($concrete, $parameters);
+        }
+
+        if ($this->isShared($abstract)) {
+            $this->instances[$abstract] = $object;
+        }
+
+        return $object;
+    }
+
+    public function get($abstract)
+    {
+        return $this->make($abstract);
+    }
+
+    protected function getConcrete($abstract)
+    {
+        if (!isset($this->bindings[$abstract])) {
+            return $abstract;
+        }
+
+        return $this->bindings[$abstract]['concrete'];
+    }
+
+    protected function isBuildable($concrete, $abstract)
+    {
+        return $concrete === $abstract || $concrete instanceof Closure;
+    }
+
+    protected function build($concrete, array $parameters = [])
+    {
+        if ($concrete instanceof Closure) {
+            return $concrete($this, $parameters);
+        }
+
+        $reflector = new ReflectionClass($concrete);
+
+        if (!$reflector->isInstantiable()) {
+            throw new Exception("Class {$concrete} is not instantiable");
+        }
+
+        $constructor = $reflector->getConstructor();
+
+        if (is_null($constructor)) {
+            return new $concrete;
+        }
+
+        $dependencies = $constructor->getParameters();
+        $instances = $this->resolveDependencies($dependencies);
+
+        return $reflector->newInstanceArgs($instances);
+    }
+
+    protected function resolveDependencies(array $dependencies)
+    {
+        $results = [];
+
+        foreach ($dependencies as $dependency) {
+            $results[] = is_null($dependency->getClass())
+                ? $this->resolvePrimitive($dependency)
+                : $this->resolveClass($dependency);
+        }
+
+        return $results;
+    }
+
+    protected function resolvePrimitive(ReflectionParameter $parameter)
+    {
+        if ($parameter->isDefaultValueAvailable()) {
+            return $parameter->getDefaultValue();
+        }
+
+        throw new Exception("Unresolvable dependency resolving [$parameter]");
+    }
+
+    protected function resolveClass(ReflectionParameter $parameter)
+    {
+        try {
+            return $this->make($parameter->getClass()->name);
+        } catch (Exception $e) {
+            if ($parameter->isOptional()) {
+                return $parameter->getDefaultValue();
+            }
+
+            throw $e;
+        }
+    }
+
+    protected function getClosure($abstract, $concrete)
+    {
+        return function ($container, $parameters = []) use ($abstract, $concrete) {
+            if ($abstract == $concrete) {
+                return $container->build($concrete);
+            }
+
+            return $container->make($concrete);
+        };
+    }
+
+    protected function isShared($abstract)
+    {
+        return isset($this->bindings[$abstract]['shared']) &&
+               $this->bindings[$abstract]['shared'] === true;
+    }
+}
